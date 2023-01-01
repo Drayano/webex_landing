64e27cee9304808641c3fb196da8491e948cdbe3
Drayano <houari.rezk.1999@gmail.com>
2023-01-01 20:01:36 +0100

    Render test

--- extractor.js
+++ extractor.js
@@ -1,3 +1,20 @@
 const params = new URLSearchParams(document.location.search);
 const code = params.get("code");
+const state = params.get("state");
 console.log(code);
+console.log(state);
+
+function sendCode() {
+    if (code != null) {
+        fetch(`https://webex-integration.onrender.com/code/${code}`, {
+            method: 'GET',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
+}
+
+sendCode();
